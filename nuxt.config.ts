943927aca2cdd9d92c8103bc661d5a5348d86e77
Xanthus Wong <lonelynok@gmail.com>
2022-07-22 02:12:43 +0800

    - basic config for webpack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  builder: 'webpack',
+  build: {
+    transpile: ['vue-demi'],
+  },
+  ssr: false,
+  webpack: {
+    extractCSS: false
+  }
 })
